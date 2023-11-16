c62f6cdf4692fe49f0f14f4d245ab36ba7707cd6
tarekmansour <tarekmansour.net@gmail.com>
2023-11-16 21:38:15 +0100

    cleanup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Profile from "./Profile";
+
+function MyButton() {
+  return <button>I'm a button</button>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+      <Profile />
     </div>
   );
 }
